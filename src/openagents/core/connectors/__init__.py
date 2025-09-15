810be12198bfd384bcb35562be22ca04a8033349
Nebu Kaga <nebu.kaga@openagents.org>
2025-09-15 20:42:37 +0000

    -

---
+++ src/openagents/core/connectors/__init__.py
@@ -0,0 +1,14 @@
+"""
+Network Connectors for OpenAgents
+
+This package provides network connectors that allow agents to connect to different
+types of networks and communication protocols.
+"""
+
+from .base import NetworkConnector
+from .grpc_connector import GRPCNetworkConnector
+
+__all__ = [
+    'NetworkConnector',
+    'GRPCNetworkConnector',
+]
