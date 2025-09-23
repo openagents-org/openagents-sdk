c7f19406bced8601aba74718ec7ce86440870884
kraneco <kraneco@163.com>
2025-09-23 16:31:54 +0000

    fix(): fix forum problems

--- studio/src/pages/wiki/WikiMainPage.tsx
+++ studio/src/pages/wiki/WikiMainPage.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { useThemeStore } from "@/stores/themeStore";
 import { useOpenAgentsService } from "@/contexts/OpenAgentsServiceContext";
 import WikiView from "@/components/wiki/WikiView";
 
@@ -8,7 +7,6 @@ import WikiView from "@/components/wiki/WikiView";
  * Wiki主页面 - 处理Wiki相关的所有功能
  */
 const WikiMainPage: React.FC = () => {
-  const { theme } = useThemeStore();
   const { service: openAgentsService } = useOpenAgentsService();
 
   return (
@@ -18,7 +16,6 @@ const WikiMainPage: React.FC = () => {
         index
         element={
           <WikiView
-            currentTheme={theme}
             connection={openAgentsService}
           />
         }
