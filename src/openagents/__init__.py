5cc46644bc4c73563b6c4492ae97713f9bf7bec9
Nebu Kaga <nebu.kaga@openagents.org>
2026-03-19 06:08:44 +0000

    chore: bump version to 0.9.3.post11

--- src/openagents/__init__.py
+++ src/openagents/__init__.py
@@ -1,5 +1,5 @@
 """OpenAgents - AI Agent Networks for Open Collaboration."""
 
-__version__ = "0.9.3.post10"
+__version__ = "0.9.3.post11"
 
 from openagents.connect import AgentIdentity, connect, connect_sync
